9e3a2eb7b37c6da6613ffaae71231f68fcabb2ff
RafaelMAia101 <rafanino431@gmail.com>
2025-04-09 15:16:40 -0300

    Add files via upload
    textando o sistema de denuncias

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request
+import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
+
+# Cria a tabela se não existir
+def init_db():
+    conn = sqlite3.connect('denuncias.db')
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS denuncias (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        texto TEXT NOT NULL,
+        data_hora TEXT
+    )
+""")
+
+    conn.commit()
+    conn.close()
 
 @app.route('/')
 def index():
@@ -8,9 +26,18 @@ def index():
 
 @app.route('/denuncia', methods=['POST'])
 def denuncia():
+    nome = request.form.get('nome')
     texto = request.form.get('denuncia')
-    print(f'Denúncia recebida: {texto}')  # Aqui você pode salvar num banco ou arquivo
+    data_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    if texto:
+        conn = sqlite3.connect('denuncias.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO denuncias (nome, texto, data_hora) VALUES (?, ?, ?)", (nome, texto, data_hora))
+        conn.commit()
+        conn.close()
     return render_template('index.html', mensagem='Denúncia enviada com sucesso!')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    init_db()
+    app.run(debug=True)
